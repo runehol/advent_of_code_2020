e7cdc18830f8795dcf8f3f51858a444e36756254
Rune Holm <rune.holm@arm.com>
2021-12-29 13:24:50 +0100

    solved day 15

---
+++ day15.exs
@@ -0,0 +1,38 @@
+#! /usr/bin/env elixir
+
+
+defmodule Day15 do
+
+  def next({turn, curr, last_seen}) do
+    turn = turn + 1
+    dist = turn-Map.get(last_seen, curr, turn)
+    last_seen = Map.put(last_seen, curr, turn)
+    {curr, {turn, dist, last_seen}}
+  end
+
+  def preload(values) do
+    Enum.reduce(values, {0, 0, %{}}, fn value, {turn, _, last_seen} ->
+      st = {turn, value, last_seen}
+      {_, st} = next(st)
+      st
+    end)
+  end
+
+  def generate(start_values) do
+    Stream.concat(start_values,
+      Stream.unfold(preload(start_values), &next/1))
+  end
+
+  def run_a do
+    IO.puts(Enum.at(generate([0,5,4,1,10,14,7]), 2020-1))
+  end
+
+  def run_b do
+    IO.puts(Enum.at(generate([0,5,4,1,10,14,7]), 30000000-1))
+  end
+
+
+end
+
+Day15.run_a()
+Day15.run_b()

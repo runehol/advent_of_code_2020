4ac78c4f7026510a3216d9562cb9e34106c540d5
Rune Holm <rune.holm@arm.com>
2021-12-29 20:35:50 +0100

    solved day 1

---
+++ day1.exs
@@ -0,0 +1,54 @@
+#! /usr/bin/env elixir
+
+
+defmodule Day1 do
+
+  defp read_data(fname) do
+    File.read!(fname)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp maybe_double_sum(a, b) do
+    if a + b == 2020 do
+      [a*b]
+    else
+      []
+    end
+  end
+
+  defp maybe_triple_sum(a, b, c) do
+    if a + b + c == 2020 do
+      [a*b*c]
+    else
+      []
+    end
+  end
+
+  def run_a do
+    numbers = read_data("day1_input.txt")
+
+    res = for a <- numbers, b <- numbers, do: maybe_double_sum(a, b)
+
+    res
+    |> Enum.concat
+    |> Enum.at(0)
+    |> IO.inspect
+  end
+
+  def run_b do
+    numbers = read_data("day1_input.txt")
+
+    res = for a <- numbers, b <- numbers, c <- numbers, do: maybe_triple_sum(a, b, c)
+
+    res
+    |> Enum.concat
+    |> Enum.at(0)
+    |> IO.inspect
+  end
+
+
+end
+
+Day1.run_a()
+Day1.run_b()

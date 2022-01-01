9d0713f1f59a6b0125b255bdf322bc4ef697aff2
Rune Holm <rune.holm@gmail.com>
2022-01-01 11:48:39 +0100

    solved day 6

---
+++ day6.exs
@@ -0,0 +1,52 @@
+#! /usr/bin/env elixir
+
+
+defmodule Day6 do
+
+
+
+
+  defp read_data(fname) do
+    File.read!(fname)
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn group ->
+      group
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn person ->
+        person
+        |> String.to_charlist
+        |> MapSet.new
+      end)
+    end)
+  end
+
+  def run_a do
+    groups = read_data("day6_input.txt")
+    groups
+    |> Enum.map(fn group ->
+      group
+      |> Enum.reduce(&MapSet.union/2)
+      |> MapSet.size
+    end)
+    |> Enum.sum
+    |> IO.puts
+
+  end
+
+
+  def run_b do
+    groups = read_data("day6_input.txt")
+    groups
+    |> Enum.map(fn group ->
+      group
+      |> Enum.reduce(&MapSet.intersection/2)
+      |> MapSet.size
+    end)
+    |> Enum.sum
+    |> IO.puts
+  end
+
+end
+
+Day6.run_a()
+Day6.run_b()
